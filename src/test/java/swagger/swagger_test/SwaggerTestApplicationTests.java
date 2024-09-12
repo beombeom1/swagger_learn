c9db4d8a0cf0b5582bbe2f347eb51814305998ff
beombeom1 <jebeam3632@naver.com>
2024-09-12 14:17:27 +0900

    swagger test 생성

---
+++ src/test/java/swagger/swagger_test/SwaggerTestApplicationTests.java
@@ -0,0 +1,13 @@
+package swagger.swagger_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwaggerTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
